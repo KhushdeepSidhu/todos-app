0963f2db093ad097fdd409b5dc8a83d6d36c151f
sidhu <khushdeep>
2024-11-11 18:35:09 -0500

    updated for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require ( 'path' )
+const path = require('path');
 
 module.exports = {
-
-    entry: [ 'babel-polyfill', './src/index.js' ],
-    output: {
-        path: path.resolve ( __dirname, 'public/scripts' ),
-        filename: 'bundle.js'
+  entry: ['core-js/stable', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public/scripts'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
     },
-
-    // babel configuratiom
-    module: {
-        rules: [ {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [ 'env' ]
-                }
-            }
-        } ]
+    devMiddleware: {
+      publicPath: '/scripts/',
     },
-
-    // webpack-dev-server configuration
-    devServer: {
-        contentBase: path.resolve ( __dirname, 'public' ),
-        publicPath: '/scripts/'
-    },
-
-    // configure source map
-    devtool: 'source-map'
-
-}
+    open: true, // To open browser automatically after running code
+    // hot: 'only',
+  },
+  devtool: 'source-map',
+};
